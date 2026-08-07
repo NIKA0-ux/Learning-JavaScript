710a8df2b8e2dc1634a58c00d1226d3b805035ee
Nika0-ux <adenijifiyinfoluwa0@gmail.com>
2026-08-07 15:05:54 +0100

    adding new file

---
+++ exercise.js
@@ -0,0 +1,72 @@
+//1. Find odd numbers (While Loop)
+
+// let i = 1;
+// while (i <= 100) {
+//     if (i % 2 !== 0) {
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+
+//2. Printing multiples of 3 (while Loop)
+
+//  let i = 1;
+
+// while (i <= 30) {
+//     console.log(`${3 * i}`);
+//     i++;
+// }
+
+
+//3. 7 x table (For Loop)
+
+// for (let i = 1; i <= 40; i++) {
+//     console.log(`7 x ${i} = ${7 * i}`);
+// }
+
+
+//4. Array of states (Print 3rd state)
+// const states = ["Ogun", "Edo", "Osun", "Oyo", "Abuja", "Lagos"];
+// console.log(states[2]);
+
+//5. Adding new state to the end
+// states.push("Ibadan");
+// console.log(states);
+
+// 6. Adding new state to the beginning
+// states.unshift("Kaduna");
+// console.log(states);
+
+// 7. Printing all states
+// for (const state of states) {
+//     console.log(state);
+// }
+
+
+// 8. Even numbers (Do While Loop)
+
+// let i = 1;
+// do {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+//     i++;
+// } while (i <= 100);
+
+
+// 9. count down from 10 (Do While Loop)
+
+// let i = 10;
+// do {
+//     console.log(i);
+//     i--;
+// } while (i >= 1);
+
+
+// 10. 9 x table (Do While Loop)
+let i = 1;
+do {
+    console.log(`9 x ${i} = ${9 * i}`);
+    i++;
+} while ( i <= 40);
